b39122bd057eb9ed24cfa1ca4e689bb909387af3
debieThomas <thomasdebie.tdb@gmail.com>
2016-05-12 09:40:48 +0200

    VerrichtingAttributes

--- H7-RekeningSaldo/Verrichting.cs
+++ H7-RekeningSaldo/Verrichting.cs
@@ -9,8 +9,31 @@ namespace H7_RekeningSaldo
     class Verrichting
     {
         //properties
+        private Double _bedrag;
 
+        public Double Bedrag
+        {
+            get { return _bedrag; }
+            set { _bedrag = value; }
+        }
 
+        private DateTime _datum;
+
+        public DateTime Datum
+        {
+            get { return _datum; }
+            set { _datum = value; }
+        }
+        private String _omschrijving;
+
+        public String Omschrijving
+        {
+            get { return _omschrijving; }
+            set { _omschrijving = value; }
+        }
+        
+        
+        
         //constructor
 
         //methods
