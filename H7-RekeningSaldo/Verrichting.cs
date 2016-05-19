ec7f6988c81191610000fe7de2c86ef42eb880ef
debieThomas <thomasdebie.tdb@gmail.com>
2016-05-19 09:23:27 +0200

    verrichting comment

--- H7-RekeningSaldo/Verrichting.cs
+++ H7-RekeningSaldo/Verrichting.cs
@@ -9,32 +9,32 @@ namespace H7_RekeningSaldo
     class Verrichting
     {
         //properties
-        private Double _bedrag;
+        private Double _bedrag;//het bedrag van de verrichting
         public Double Bedrag
         {
             get { return _bedrag; }
             set { _bedrag = value; }
         }
 
-        private DateTime _datum;
+        private DateTime _datum;//de datum van de verrichting
         public DateTime Datum
         {
             get { return _datum; }
             set { _datum = value; }
         }
 
-        private String _omschrijving;
+        private String _omschrijving;//de omschrijving van de verrichting
         public String Omschrijving
         {
             get { return _omschrijving; }
             set { _omschrijving = value; }
         }       
         //constructor
-        public Verrichting()
+        public Verrichting()//lege verrichting
         {
 
         }
-        public Verrichting(Double bedrag, DateTime datum, String omschrijving )
+        public Verrichting(Double bedrag, DateTime datum, String omschrijving) //Constructor voor verrichting met alle nodige eigenschappen
         {
             Omschrijving = omschrijving;
             Datum = datum;
@@ -44,8 +44,8 @@ namespace H7_RekeningSaldo
         public override string ToString()
         {
             //herschrijven van de ToString() method
-            string datumnieuw = Datum.Day + "-" + Datum.Month + "-" + Datum.Year;
-            return Omschrijving + " (€" + Bedrag.ToString("0.00") + ")" + " op " + datumnieuw;
+            string datumnieuw = Datum.Day + "-" + Datum.Month + "-" + Datum.Year; //datum juist formatteren
+            return Omschrijving + " (€" + Bedrag.ToString("0.00") + ")" + " op " + datumnieuw; //De verrichting op een leesbare manier in de list verwerken
         }
     }
 }
