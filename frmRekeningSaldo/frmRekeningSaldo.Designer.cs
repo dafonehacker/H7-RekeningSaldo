49f8c60a4751f7d83b146a37fc908abc85fc5e40
debieThomas <thomasdebie.tdb@gmail.com>
2016-05-19 09:56:16 +0200

    dunnyshiz

--- frmRekeningSaldo/frmRekeningSaldo.Designer.cs
+++ frmRekeningSaldo/frmRekeningSaldo.Designer.cs
@@ -153,6 +153,8 @@
             this.Controls.Add(this.lblStand);
             this.Controls.Add(this.lblStandText);
             this.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
             this.Name = "frmRekeningSaldo";
             this.Text = "Rekening";
             this.ResumeLayout(false);
